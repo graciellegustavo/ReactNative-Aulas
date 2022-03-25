91008dced95e4cbddab52333fedb20115dc54381
‘ola’ <ola>
2022-03-25 19:00:41 -0300

    terminamos apostila 03

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles.css'
 
 const App = () => {
+    const obterTextoDoBotao = () => "Enviar"
+    const textoDoRotulo = "Nome:"
+    const estiloBotao = { marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'brown', width: '100%', borderStyle: 'none', borderRadius: 8 }
     return (
         <div style={{ margin: 'auto', width: '80%', backgroundColor: 'lightblue', padding: 12, borderRadius: 8 }}>
-            <label htmalFor = "nome" style={{display: 'block', marginBottom:4}} >
-                Meu primeiro componente React.
-            </label> 
-            <input id="nome" type="text" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', borderRadius: 8, width: '100%', boxSizing: 'border-box'}}/>
-            <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'brown', width: '100%', borderStyle: 'none', borderRadius: 8}}>Enviar</button>
+            <label className="rotulo" style={{ display: 'block', marginBottom: 4 }} >
+                {textoDoRotulo}
+            </label>
+            <input id="nome" type="text" style={{ paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', borderRadius: 8, width: '100%', boxSizing: 'border-box' }} />
+            <button style={estiloBotao}>{obterTextoDoBotao()}</button>
         </div>
     )
 }

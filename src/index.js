bbc0cffede728977407b61b55a35c2069c9566f5
‘ola’ <ola>
2022-03-25 18:43:32 -0300

    transformando em jsx

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     return (
-            <div>
-                Meu primeiro componente React.        
-            </div>
-        )
+        <div style={{ margin: 'auto', width: '80%', backgroundColor: 'lightblue', padding: 12, borderRadius: 8 }}>
+            <label htmalFor = "nome" style={{display: 'block', marginBottom:4}} >
+                Meu primeiro componente React.
+            </label> 
+            <input id="nome" type="text" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', borderRadius: 8, width: '100%', boxSizing: 'border-box'}}/>
+            <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'brown', width: '100%', borderStyle: 'none', borderRadius: 8}}>Enviar</button>
+        </div>
+    )
 }
 
 ReactDOM.render(
     <App />,
-    document.querySelector("#root")   
+    document.querySelector("#root")
 )
